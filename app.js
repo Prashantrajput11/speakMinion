8a563f66b1b0c563da06165861a82b94f29de64e
Prashant Tanwar <69415658+Prashantrajput11@users.noreply.github.com>
2022-01-11 22:26:03 +0530

    feat: converted code to es6

--- app.js
+++ app.js
@@ -1,42 +1,39 @@
+
+// Reference all the necessary tags
 let btn = document.querySelector(".btn");
-let input=document.querySelector(".input-box")
-let output=document.querySelector(".output-box")
+let input = document.querySelector(".input-box")
+let output = document.querySelector(".output-box")
+
+
 // var serverUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-var serverUrl="https://api.funtranslations.com/translate/minion.json"
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
 
-function getUrl(text)
-{
-return serverUrl + "?" +"text="  +text
-}
-// getUrl("i am crazy")
+// construct url by appending the user's text
+let getUrl = text => serverUrl + "?" + "text=" + text
 
 
+// Get the data (fetched by url) on click on the button.
+let getData = () =>{
 
-btn.addEventListener("click",function(){
+    let inputText = input.value
 
-console.log("working");
-var inputText=input.value 
-
-
-// fetch(getUrl(inputText))
-// .then(response=>response.json())
-// .then(json=>{var outputText=json.contents.translated
-//     output.value=outputText});
-
+    // Fetch data
     fetch(getUrl(inputText))
-    .then(function(response){
-return response.json()
-    })
-    .then(function(data){
-var outputText=data.contents.translated
-output.value=outputText
-    })
-    .catch(errorHandler)
-});
+    .then(
+        response => response.json() // converts the response in readable format
+    )
+    .then(
+        json => {
+            let outputText = json.contents.translated
+            output.value = outputText
+        }
+    )
+}
 
-function errorHandler(error){
-    console.log("oops! somthing went wrong" ,error)
-   }
+// Event handler 
+btn.addEventListener("click", getData)
+
+
 
 
 
